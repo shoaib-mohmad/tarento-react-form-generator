44f1ed0a47a082c66793d9432672ea14e743f477
shoaib-mohmad <shoaib.mohmad@tarento.com>
2020-04-17 14:52:50 +0530

    Updates

---
+++ precache-manifest.b49e32511da283b497ae080bc0bc5420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056a835813ff9c94e756de52d865a51e",
+    "url": "/tarento-react-form-generator/index.html"
+  },
+  {
+    "revision": "420e337ebc356baeefc8",
+    "url": "/tarento-react-form-generator/static/css/main.0a26a5c9.chunk.css"
+  },
+  {
+    "revision": "dc449b4974afb62e5cfd",
+    "url": "/tarento-react-form-generator/static/js/2.149b30c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarento-react-form-generator/static/js/2.149b30c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420e337ebc356baeefc8",
+    "url": "/tarento-react-form-generator/static/js/main.0d8fbf42.chunk.js"
+  },
+  {
+    "revision": "75f2a6b1653ba24f7a0e",
+    "url": "/tarento-react-form-generator/static/js/runtime-main.91572685.js"
+  }
+]);
